09ac3ea7494ec6b7f8e992d99d56e5452e539cc7
Suzii <z.dankovcikova@gmail.com>
2017-08-02 20:26:50 +0200

    Enable vanitized urls on localhost

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,53 @@ const gulp = require('gulp');
 const path = require('path');
 const pug = require('gulp-pug');
 const less = require('gulp-less');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
 
 const OUTPUT_DIR = './dist';
 
-function swallowError (error) {
+function swallowError(error) {
     console.log(error.message.toString());
     this.emit('end')
 }
 
-gulp.task('pug', function(){
+gulp.task('pug', function () {
     gulp.src([
         'src/pug/pages/*.pug',
         'src/pug/pages/jachting/*.pug'
     ])
-        .pipe(pug({pretty: true}))
+        .pipe(pug({ pretty: true }))
         .on('error', swallowError)
         .pipe(gulp.dest(OUTPUT_DIR))
-        .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('less', function(){
+gulp.task('less', function () {
     return gulp.src('src/less/styles.less')
         .pipe(less())
         .on('error', swallowError)
         .pipe(gulp.dest(OUTPUT_DIR + '/css'))
-        .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('browser-sync', ['less', 'pug'],function(){
-    browserSync({
-        server: {baseDir: OUTPUT_DIR},
-        notify: true,
-        serveStatic: ['.', './img']
-    })
+gulp.task('browser-sync', ['less', 'pug'], function () {
+    browserSync.init({
+        server: {
+            baseDir: OUTPUT_DIR,
+            notify: true,
+            serveStatic: ['.', './img'],
+            serveStaticOptions: {
+                extensions: ['html']
+            }
+        }
+    });
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     gulp.watch(['src/less/**/*.less'], ['less']);
     gulp.watch(['src/pug/**/*.pug'], ['pug']);
 });
 
-gulp.task('copy-assets', function() {
+gulp.task('copy-assets', function () {
     gulp.src('./img/**/*')
         .pipe(gulp.dest(OUTPUT_DIR + '/img'))
 });
