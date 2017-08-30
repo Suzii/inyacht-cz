72a7cbffe0dd380a10790d191032910a11c596fd
Suzii <z.dankovcikova@gmail.com>
2017-08-30 22:55:57 +0200

    Do not render smartsupp in debug mode

--- routes/helpers/modelBuilder.js
+++ routes/helpers/modelBuilder.js
@@ -1,9 +1,13 @@
 const sitemap = require('./sitemap');
+const debug = require('debug')('inyacht-cz:server');
 
 const getSharedModel = (currentRouteId) => ({
+    debug: debug,
+
     sitemap: sitemap,
     currentRouteId: sitemap[currentRouteId].id,
     title: sitemap[currentRouteId].title,
+
 });
 
 module.exports = getSharedModel;
