d629973908530216f6ca064e4259566a540d03f9
Suzii <z.dankovcikova@gmail.com>
2017-10-22 16:26:08 +0200

    Render data from kentico cloud for F&Q page

--- routes/kentico-cloud/models/QnA.js
+++ routes/kentico-cloud/models/QnA.js
@@ -15,7 +15,7 @@ class QnA extends ContentItem {
 
         return fieldName;
       }),
-      linkResolver: (link) => link.type === 'q___a' ? `faq#${link.url_slug}` : undefined,
+      linkResolver: (link) => link.type === 'q___a' ? `/faq#${link.url_slug}` : undefined,
     })
   }
 }
