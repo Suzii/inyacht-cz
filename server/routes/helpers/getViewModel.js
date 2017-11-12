f95990300b2f9879286982761856fbcbfe1bf056
Suzii <z.dankovcikova@gmail.com>
2017-11-12 11:08:20 +0100

    Split BM css to multiple files and play with it a bit

--- server/routes/helpers/getViewModel.js
+++ server/routes/helpers/getViewModel.js
@@ -3,6 +3,7 @@ const debug = require('debug')('inyacht-cz:server');
 
 const getViewModel = (currentRouteId, item, more) => (Object.assign({
   debug: debug,
+  bookingCss: 'https://inyacht-preview.herokuapp.com/css/booking-manager.css',
 
   sitemap: sitemap,
   currentRouteId: sitemap[currentRouteId].id,
