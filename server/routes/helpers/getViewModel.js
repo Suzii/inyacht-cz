6e458814824e8411e27cb79cbfaf5dcb2f6f7fee
Suzii <z.dankovcikova@gmail.com>
2018-01-07 15:37:11 +0100

    Add LOCAL and LIVE_DOMAIN config values

--- server/routes/helpers/getViewModel.js
+++ server/routes/helpers/getViewModel.js
@@ -3,8 +3,9 @@ const debug = require('debug')('inyacht-cz:server');
 
 const getViewModel = (currentRouteId, item, more) => (Object.assign({
   debug: debug,
-  // bookingCss: 'https://localhost:3000/css/booking-manager.css',
-  bookingCss: 'https://inyacht.herokuapp.com/css/booking-manager.css',
+  bookingCssLg: `${process.env.LIVE_DOMAIN}/css/booking-manager.css`,
+  bookingCssSm: `${process.env.LIVE_DOMAIN}/css/booking-manager-small.css`,
+  controllerPage: process.env.local ? 'http://localhost:3000/najit-lod' : `${process.env.LIVE_DOMAIN}/najit-lod`,
 
   sitemap: sitemap,
   currentRouteId: sitemap[currentRouteId].id,
